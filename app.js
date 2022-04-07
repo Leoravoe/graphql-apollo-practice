f676fbe0732c6845149656432bbfdf6b79da50a3
Preetam Sarkar <sarkarpreetam12@gmail.com>
2022-04-08 01:09:45 +0530

    graphql setup and type query written

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
 const express = require('express');
-const res = require('express/lib/response');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql')
 
 
 
 const app = express();
+
+const schema = buildSchema(`
+    type User{
+        name: String
+        age: Int
+        college: String
+    }
+    type Query{
+        hello: String
+        user: User
+        name(n: String!): String!
+    }
+`)
+
+const root = {
+    hello: ()=>{
+        return "Hi There"
+    },
+    user: ()=>{
+        const user = {
+            name : "Preetam Sarkar",
+            age : 56,
+            college : "BIT"
+        }
+        return user;
+    },
+    name: (n)=>{
+        // console.log(n)
+        return n.n;
+    }
+}
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema : schema,
+    rootValue: root
+}));
 
 app.listen(4000,()=>{
     console.log('Listening to port 4000');
