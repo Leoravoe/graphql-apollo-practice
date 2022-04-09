b378edbd3d1a08de8a8a64d2d5ebeb64d6c67604
Preetam Sarkar <sarkarpreetam12@gmail.com>
2022-04-09 20:30:20 +0530

    post.json and schema changes done

--- graphQLSchema/schema.js
+++ graphQLSchema/schema.js
@@ -2,11 +2,19 @@ const { gql } = require('apollo-server-express')
 
 
 const typeDefs = gql`
+    type User{
+        name: String!
+    }
+    type Comment{
+        user: User!
+        body: String!
+    }
     type Post{
         userId: Int!
         id: ID!
         title: String!
         body: String!
+        comment: [Comment]
     }
     type Query{
         getPost: [Post]
