3cfc799089700c4bff52576924d089a4139630fb
Preetam Sarkar <sarkarpreetam12@gmail.com>
2022-04-15 05:26:31 +0530

    worked with parent resolvers

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,12 +1,19 @@
 const data = require('../demoPost.json')
+// console.log(data)
 
 
 const resolvers = {
     Query: {
-        getPost : ()=>{
+        getPost : (parent,args,context,info)=>{
             return data;
         }
-    }
+    },
+    Post: {
+        comment: (parents,args,context,info)=>{
+            console.log(parents.comment);
+            return parents.comment
+        }
+    },
 }
 
 module.exports = resolvers
